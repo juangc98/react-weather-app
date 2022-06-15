dfc18bc8e9cd8fcf6920ba8eea0a479674d94710
Juan Gómez Carrillo <juangc_98@hotmail.com>
2022-06-15 10:29:39 -0300

    Displaying forecast hourly

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,30 @@ import LoadSpinner from './LoadSpinner';
 
 const Card = ({loadingData, setData, weather, forecast}) => {
 
-    //
+    //Neccesary variables
     let today = new Date();
     let day = today.getDate();
     let month = today.getMonth();
     let year = today.getFullYear();
     let date = day + '/' + month + '/' + year ;
-    //
+    // WEATHER
     let url = "";
     let iconUrl = "";
+    // FORECAST
+    let actualHour = today.getHours();
+    let lastOfDay = ((24 - actualHour) / 3).toFixed(0);
+    const allfc = forecast.list;
+    let fc1 = "";
+    let fc2 = "";
+    let fc3 = "";
+    let fc4 = "";
+    let fc5 = "";
+    let fdate1 = "";
+    let fdate2 = "";
+    let fdate3 = "";
+    let fdate4 = "";
+    let fdate5 = "";
+
 
     // When Loading....
     if(loadingData) {
@@ -23,9 +38,21 @@ const Card = ({loadingData, setData, weather, forecast}) => {
 
     //Get Icon
     if(setData){
+        //WEATHER DATA
         url = "http://openweathermap.org/img/w/";
-        iconUrl = url + weather.weather[0].icon + ".png"
+        iconUrl = url + weather.weather[0].icon + ".png";
+        // FORECAST
 
+        fc1 = allfc.slice(0, lastOfDay);
+        // fc2 = allfc.slice(lastOfDay, lastOfDay + 8);
+        // fc3 = allfc.slice(lastOfDay + 8, lastOfDay + 16);
+        // fc4 = allfc.slice(lastOfDay + 16, lastOfDay + 24);
+        // fc5 = allfc.slice(lastOfDay + 24, lastOfDay + 32);
+        fdate1 = fc1[0].dt_txt.substring(8, 10) + "/" + fc1[0].dt_txt.substring(5, 7);
+        // fdate2 = fc2[0].dt_txt.substring(8, 10) + "/" + fc2[0].dt_txt.substring(5, 7);
+        // fdate3 = fc3[0].dt_txt.substring(8, 10) + "/" + fc3[0].dt_txt.substring(5, 7);
+        // fdate4 = fc4[0].dt_txt.substring(8, 10) + "/" + fc4[0].dt_txt.substring(5, 7);
+        // fdate5 = fc5[0].dt_txt.substring(8, 10) + "/" + fc5[0].dt_txt.substring(5, 7);
     }
     // CARD Data-Visualization
     return (
@@ -34,59 +61,106 @@ const Card = ({loadingData, setData, weather, forecast}) => {
             {
                 setData === true ? (
                     <div className='w-full h-full flex flex-nowrap flex-col rounded-xl shadow-xl overflow-hidden' >
-                        <div className='w-full h-full p-5 bg-white'>
+                        <div className='w-full h-full p-5 bg-white grid grid-cols-1 md:grid-cols-2'>
 
-                            <div className='flex flex-row'>                                
+                            <div className='first-col flex flex-col h-full justify-between'>
+
                                 <div className='text-md lg:text-base !leading-snug'>
                                     <p>Actualmente</p>
                                     <h3>{weather.name}, {weather.sys.country}</h3>
                                 </div>
-                            </div>
 
-                            
-                            <div className='flex flex-row items-stretch'>
-                                <h1 id='sense-temp' className='text-6xl lg:text-7xl font-semibold w-max'>
-                                    {(weather.main.feels_like - 273.15).toFixed(1)}
-                                </h1>
-                                <div className='flex flex-col justify-around pl-1'>
-                                    <span className='w-2 h-2 border-2 border-black rounded-full pb-1'></span>
-                                    <span className='text-xl lg:text-3xl  leading-none'>C</span>
-                                </div>
-                            </div>
-
-                            <div className='flex flex-row items-center gap-3 -mt-2'>
-                                <div className='flex flex-row-reverse items-center'>
-                                    <p>"{weather.weather[0].description}"</p>
-                                    <div className='w-16 h-16 -m-1 flex relative justify-center items-center'>
-                                        <img className='w-full h-full object-center object-contain' src={ iconUrl } alt="icon weather" />
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div className='flex flex-row flex-nowrap justify-between items-end pt-5'>
-                                 <ul className='text-md lg:text-base !leading-snug'>
-                                    <div className='flex flex-row items-center gap-x-1'>
-                                        <div className='text-2xl'>
-                                            🌡️
+                                <div className='flex flex-col'>
+                                    <div className='flex items-center'>
+                                        <div className='w-16 h-16 -m-3 flex relative justify-center items-center'>
+                                            <img className='w-full h-full object-center object-contain' src={ iconUrl } alt="icon weather" />
                                         </div>
-                                        <div>
-                                            <li className='font-bold'>{(weather.main.temp - 273.15).toFixed(1)} <span className='text-'>°C</span>  </li>
-                                            <li className='text-sm'>{(weather.main.temp_min - 273.15).toFixed(1)} <span className='text-'>°C</span> / {(weather.main.temp_max - 273.15).toFixed(1)} <span className='text-'>°C</span>  </li>
+                                        <p>"{weather.weather[0].description}"</p>
+                                    </div>
+                                    <div className='flex flex-row items-stretch'>
+                                        <h1 id='sense-temp' className='text-6xl lg:text-7xl font-semibold w-max'>
+                                            {(weather.main.feels_like - 273.15).toFixed(1)}
+                                        </h1>
+                                        <div className='flex flex-col justify-around pl-1'>
+                                            <span className='w-2 h-2 border-2 border-black rounded-full pb-1'></span>
+                                            <span className='text-xl lg:text-3xl  leading-none'>C</span>
                                         </div>
                                     </div>
+                                    
+                                </div>
+
+                            </div>
+                            
+                            <div className='secon-col flex flex-row md:flex-col flex-nowrap justify-between md:justify-end items-end text-left md:text-right pt-5'>
+                                 <ul className='text-md lg:text-base !leading-snug flex flex-col'>
+                                    <li className='inline-flex items-center font-bold'> 🌡️{(weather.main.temp - 273.15).toFixed(1)} <span className='text-'>°C</span>  </li>
+                                    <li className='inline-flex items-center text-sm'> <span className='!leading-none font-bold !text-xl'> - </span> {(weather.main.temp_min - 273.15).toFixed(1)} <span className='text-'>°C</span> </li>
+                                    <li className='inline-flex items-center text-sm'> <span className='!leading-none font-bold !text-xl'> + </span> {(weather.main.temp_max - 273.15).toFixed(1)} <span className='text-'>°C</span>  </li>
                                     <li className=''>☁️ {weather.clouds.all}% </li>
-                                    <li className=''>💧 {weather.main.humidity}% </li>
-                                    <li className=''>💨 {(weather.wind.speed).toFixed(2)} <span className='text-'>m/s</span>  </li>
                                 </ul>
                                 <ul className='text-md lg:text-base !leading-snug text-right'>
+                                    <li className=''>💧 {weather.main.humidity}% </li>
+                                    <li className=''>💨 {(weather.wind.speed).toFixed(2)} <span className='text-'>m/s</span>  </li>
                                     <li className=''>☀️ {weather.sys.sunrise}</li>
                                     <li className=''>🌒 {weather.sys.sunset}</li>
                                 </ul>
                             </div>
-
                             
                         </div>
-                        <div className='w-full h-full p-5 bg-gray-500'></div>
+                        <div className='w-full h-full p-5 bg-gray-500 border-t border-gray-400'>
+                            <div className='w-full h-full relative'>
+                                <div className='absolute left-0 right-0 bottom-0 overflow-x-scroll'>
+                                    <div className=''>
+                                        <p className=''>Hoy {fdate1}</p>
+                                        <div className='grid grid-cols-8 gap-x-2 my-1'>
+                                            {fc1.map(({ main, weather, dt_txt, dt }) => (
+                                                <div key={dt} className='flex flex-col justify-start items-center'>
+                                                    <span className='text-sm'> {(main.temp- 273.15).toFixed(1)} </span>
+                                                    <span className='-m-2'>
+                                                        <img className='w-full h-full object-center object-contain' src={url + weather[0].icon + ".png"}  alt="icon weather" />
+                                                    </span>
+                                                    <div className='h-0.5 w-3/4 bg-gray-400 rounded-full'></div>
+                                                    <span className='text-sm'> {dt_txt.substring(11, 16)} </span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+
+                                    <div className=''>
+                                        <p className=''>Hoy {fdate1}</p>
+                                        <div className='grid grid-cols-8 gap-x-2 my-1'>
+                                            {fc1.map(({ main, weather, dt_txt, dt }) => (
+                                                <div key={dt} className='flex flex-col justify-start items-center'>
+                                                    <span className='text-sm'> {(main.temp- 273.15).toFixed(1)} </span>
+                                                    <span className='-m-2'>
+                                                        <img className='w-full h-full object-center object-contain' src={url + weather[0].icon + ".png"}  alt="icon weather" />
+                                                    </span>
+                                                    <div className='h-0.5 w-3/4 bg-gray-400 rounded-full'></div>
+                                                    <span className='text-sm'> {dt_txt.substring(11, 16)} </span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+
+                                    <div className=''>
+                                        <p className=''>Hoy {fdate1}</p>
+                                        <div className='grid grid-cols-8 gap-x-2 my-1'>
+                                            {fc1.map(({ main, weather, dt_txt, dt }) => (
+                                                <div key={dt} className='flex flex-col justify-start items-center'>
+                                                    <span className='text-sm'> {(main.temp- 273.15).toFixed(1)} </span>
+                                                    <span className='-m-2'>
+                                                        <img className='w-full h-full object-center object-contain' src={url + weather[0].icon + ".png"}  alt="icon weather" />
+                                                    </span>
+                                                    <div className='h-0.5 w-3/4 bg-gray-400 rounded-full'></div>
+                                                    <span className='text-sm'> {dt_txt.substring(11, 16)} </span>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 ) : (
                     <h2 className="text-gray-200" >No hay datos disponibles.</h2>
